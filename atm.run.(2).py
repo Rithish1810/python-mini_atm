05421efa3ba324ceaecc71cdf30b83dc2f56541f
Rithish1810 <rithurithish1810@gmail.com>
2026-09-12 09:58:21 +0530

    Add files via upload

---
+++ atm.run.(2).py
@@ -0,0 +1,24 @@
+Python 3.14.5 (tags/v3.14.5:5607950, May 10 2026, 10:43:50) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:\Users\Tamizh\OneDrive\Documents\python\2nd project.(atm machine).py
+enter your pin number:1234
+your pin was correct
+withdraw/deposit/check balance:withdraw
+enter your withdraw ammount:5000
+balance: 5000
+sucessfully your amount was widhdrawed collecy your cash
+>>> 
+= RESTART: C:\Users\Tamizh\OneDrive\Documents\python\2nd project.(atm machine).py
+enter your pin number:1234
+your pin was correct
+withdraw/deposit/check balance:deposit
+enter you deposit amount:1000
+11000
+your amount was successfully debited
+>>> 
+= RESTART: C:\Users\Tamizh\OneDrive\Documents\python\2nd project.(atm machine).py
+enter your pin number:1234
+your pin was correct
+withdraw/deposit/check balance:check balance
+your balance amount: 10000
